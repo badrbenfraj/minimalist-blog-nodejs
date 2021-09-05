a71d0591a33b5cee44fc8a8301a44ff551c97123
Badredine Benfraj <badrbenfraj@mail.com>
2021-09-05 18:35:09 +0100

    fix(): app port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const articleRouter = require('./routes/articles')
+const port = process.env.PORT || 5000
 app.set('view engine', 'ejs');
 
 app.use('/articles', articleRouter);
@@ -9,4 +10,6 @@ app.get('/', (req, res)=> {
     res.render('index.ejs');
 })
 
-app.listen(5000)
+app.listen(port, ()=> {
+    console.log(`App listening at http://localhost:${port}`)
+})
