286d0f776a0727f215d3046fb03624aa6f9cb6c3
Badredine Benfraj <badrbenfraj@mail.com>
2021-09-18 16:19:49 +0100

    improve(): add improvements to ejs partial files

--- models/article.js
+++ models/article.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const marked = require('marked');
+const slugify = require('slugify');
 
-const articleSchema = mongoose.Schema({
+
+const articleSchema = new mongoose.Schema({
     title: {
         required: true,
         type: String
@@ -16,7 +19,19 @@ const articleSchema = mongoose.Schema({
     creationDate: {
         type: Date,
         default: Date.now
+    },
+    slug:{
+        type: String, 
+        required: true,
+        unique: true
     }
+})
+
+articleSchema.pre('validate', function(next){
+    if(this.title){
+        this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+    next()
 })
 
 module.exports = mongoose.model('Article', articleSchema)
--- server.js
+++ server.js
@@ -1,46 +1,42 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
+const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
 const port = process.env.PORT || 5000;
 
 mongoose.connect('mongodb+srv://admin:admin@minimalist-blog-nodejs.ty2qv.mongodb.net/minimalist-blog-nodejs?retryWrites=true&w=majority')
+let db = mongoose.connection;
 
+// Check db connection
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Check for db errors
+db.on('error', (error) => {
+    console.error('DB Error: ', error);
+})
+
+// parse application form url encoded
 app.use(express.urlencoded({extended: false}))
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "ejs");
 
 app.use("/articles", articleRouter);
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "blog1",
-      creationDate: new Date(),
-      description:
-        "description description description description description description description description description description description ",
-    },
-    {
-      title: "blog2",
-      creationDate: new Date(),
-      description:
-        "description1 description1 description1 description1 description1 description1 description1 description1 description1 description1 description1 ",
-    },
-    {
-      title: "blog3",
-      creationDate: new Date(),
-      description:
-        "description1 description1 description1 description1 description1 description1 description1 description1 description1 description1 description1 ",
-    },
-    {
-      title: "blog4",
-      creationDate: new Date(),
-      description:
-        "description1 description1 description1 description1 description1 description1 description1 description1 description1 description1 description1 ",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({creationDate: -1})
   res.render("articles/index", { articles: articles });
 });
+
+app.use((req, res)=> {
+    res.status(404).render('404');
+})
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
